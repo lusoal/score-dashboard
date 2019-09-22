62105774f2485dc283612b2f87681b2f3bd9565c
gb-lucas-duarte <lucas.duarte@guiabolso.com.br>
2019-09-22 20:49:53 -0300

    Ajustado import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,3 @@
 --adicione um usuário
-insert into tb_jogador (usuario, senha, SESSAO_STATUS) values ('leozao', 'admin', false)
-insert into tb_jogador (usuario, senha, SESSAO_STATUS) values ('lucas', 'admin', false)
-insert into tb_jogador (usuario, senha, SESSAO_STATUS) values ('teste', 'admin', false)
-insert into tb_jogador (usuario, senha, SESSAO_STATUS) values ('gol', 'admin', false)
 INSERT INTO temas VALUES(1, "Sistemas Operacionais");
-INSERT INTO perguntas VALUES(1, "Envia carga x", "Envia carga y", "Envia carga j","Envia carga z", "Como funciona Round Robing", "A", 1, "https://i.gifer.com/Uzg1.gif");
-INSERT INTO perguntas VALUES(2, "Envia carga x", "Envia carga y", "Envia carga j","Envia carga z", "Como funciona Round Robing", "A", 1, "https://i.gifer.com/Uzg1.gif");
-
 --insert into tb_partidas (id, status) values (1, true)
